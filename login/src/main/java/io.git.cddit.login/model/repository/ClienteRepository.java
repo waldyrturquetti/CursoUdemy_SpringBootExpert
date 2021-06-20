7ecd0ce75f7398e5874aeaa960348fd28b753cc5
Waldyr Turquetti <44614612+waldyrturquetti@users.noreply.github.com>
2021-06-20 15:27:44 -0300

    Delete login directory

--- login/src/main/java/io.git.cddit.login/model/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package io.git.cddit.login.model.repository;
-
-import io.git.cddit.login.model.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
